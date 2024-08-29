126681000a38ed4c39c9a72a7149bc1d968a2818
Qalib <qalibismayilli8@gmail.com>
2024-08-29 15:36:11 +0400

    added some endpoints

--- src/main/java/com/company/library/api/UserController.java
+++ src/main/java/com/company/library/api/UserController.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController("/api/v1/user")
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
 
     private final UserService userService;
